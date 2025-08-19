1ae4f4982602b452d6622e9401806df266806457
madnywebtech <madnywebtech@outlook.com>
2025-08-20 00:24:51 +0500

    updated social links

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,3 +1,4 @@
+import { Facebook, Instagram } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -73,31 +74,32 @@ export default function Footer() {
         <div className="max-w-80">
           <p className="text-xl font-bold text-white ">Follow Us</p>
           <div className="flex items-center gap-3 mt-4">
-            {/* Instagram */}
-            <svg
-              className="w-6 h-6 hover:scale-95 hover:cursor-pointer"
-              fill="currentColor"
-              viewBox="0 0 24 24"
+            <a
+              href="https://www.facebook.com/profile.php?id=61579088861667"
+              className=" transition-colors"
+              target="_blank"
             >
-              <path d="M7.75 2A5.75 5.75 0 002 7.75v8.5A5.75 5.75 0 007.75 22h8.5A5.75 5.75 0 0022 16.25v-8.5A5.75 5.75 0 0016.25 2h-8.5zM4.5 7.75A3.25 3.25 0 017.75 4.5h8.5a3.25 3.25 0 013.25 3.25v8.5a3.25 3.25 0 01-3.25 3.25h-8.5a3.25 3.25 0 01-3.25-3.25v-8.5zm9.5 1a4 4 0 11-4 4 4 4 0 014-4zm0 1.5a2.5 2.5 0 102.5 2.5 2.5 2.5 0 00-2.5-2.5zm3.5-.75a.75.75 0 11.75-.75.75.75 0 01-.75.75z" />
-            </svg>
-            {/* Facebook */}
-            <svg
-              className="w-8 h-8 hover:scale-95 hover:cursor-pointer"
-              fill="currentColor"
-              viewBox="0 0 24 24"
+              <Facebook size={20} />
+            </a>
+            <a
+              href="https://www.instagram.com/hfwg2025/"
+              className=" transition-colors"
+              target="_blank"
             >
-              <path d="M13.5 9H15V6.5h-1.5c-1.933 0-3.5 1.567-3.5 3.5v1.5H8v3h2.5V21h3v-7.5H16l.5-3h-3z" />
-            </svg>
-
-            {/* LinkedIn */}
-            <svg
-              className="w-6 h-6 hover:scale-95 hover:cursor-pointer"
-              fill="currentColor"
-              viewBox="0 0 24 24"
+              <Instagram size={20} />
+            </a>
+            <a
+              href="https://www.tiktok.com/@hfwg2025"
+              className=" transition-colors"
+              target="_blank"
             >
-              <path d="M4.98 3.5C3.88 3.5 3 4.38 3 5.48c0 1.1.88 1.98 1.98 1.98h.02c1.1 0 1.98-.88 1.98-1.98C6.98 4.38 6.1 3.5 4.98 3.5zM3 8.75h3.96V21H3V8.75zm6.25 0h3.8v1.68h.05c.53-.98 1.82-2.02 3.75-2.02 4.01 0 4.75 2.64 4.75 6.07V21H17v-5.63c0-1.34-.03-3.07-1.88-3.07-1.88 0-2.17 1.47-2.17 2.98V21H9.25V8.75z" />
-            </svg>
+              <Image
+                src={"/images/icons/tiktok.png"}
+                width={60}
+                height={60}
+                className="w-5 h-5"
+              />
+            </a>
           </div>
         </div>
       </div>
--- src/app/sections/TopBar.js
+++ src/app/sections/TopBar.js
@@ -1,4 +1,5 @@
 import { Phone, Mail, Facebook, Instagram, Linkedin } from "lucide-react";
+import Image from "next/image";
 
 const TopBar = () => (
   <div className={`bg-primary-gradient !text-white backdrop-blur-sm py-2 text-sm border-b border-primary  dark:!text-white`}>
@@ -6,28 +7,28 @@ const TopBar = () => (
       <div className="flex space-x-6">
         <a
           href="mailto:hfwg2025@gmail.com"
-          className="flex items-center hover:text-primary transition-colors"
+          className="flex items-center  transition-colors"
         >
           <Mail className="h-4 w-4 mr-1" />
           hfwg2025@gmail.com
         </a>
         <a
           href="tel:+15877003395"
-          className="flex items-center hover:text-primary transition-colors"
+          className="flex items-center  transition-colors"
         >
           <Phone className="h-4 w-4 mr-1" />
           587-700-3395
         </a>
       </div>
       <div className="hidden lg:flex space-x-4 mt-2 md:mt-0 ">
-        <a href="#" className="hover:text-primary transition-colors">
+        <a href="https://www.facebook.com/profile.php?id=61579088861667" target="_blank" className=" transition-colors">
           <Facebook size={20} />
         </a>
-        <a href="#" className="hover:text-primary transition-colors">
+        <a href="https://www.instagram.com/hfwg2025/" target="_blank" className=" transition-colors">
           <Instagram size={20} />
         </a>
-        <a href="#" className="hover:text-primary transition-colors">
-          <Linkedin size={20} />
+        <a href="https://www.tiktok.com/@hfwg2025" target="_blank" className=" transition-colors">
+          <Image src={'/images/icons/tiktok.png'} width={60} height={60} className="w-5 h-5" />
         </a>
       </div>
     </div>
